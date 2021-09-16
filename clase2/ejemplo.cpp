a216705ab06edec1795845bd752f45ed2fd5cf23
Nicolas Rueda <nicolasthrash25@hotmail.com>
2021-09-16 17:22:29 -0500

    se anhade ejemplo cmath y gitignore

--- clase2/ejemplo.cpp
+++ clase2/ejemplo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 
 int main(int argc, char const *argv[])
 {
 	std::cout <<"Hello world\n";
 	std::cout <<"Bye world\n";
+	std::cout << "sin(2.3):"<<std::sin(2.3) << "\n";
 	return 0;
 }
