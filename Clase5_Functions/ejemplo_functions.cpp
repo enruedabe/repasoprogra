8804200a1372f192ed4700a20d5c12f543776111
Nicolas Rueda <nicolasthrash25@hotmail.com>
2021-09-20 18:35:24 -0500

    Se anhade primer ejemplo de functions

---
+++ Clase5_Functions/ejemplo_functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void print_message(int a){
+	std::cout <<"this is a message"<<std::endl;
+	std::cout <<"Parametro a:"<< a <<std::endl;
+	std::cout <<"Direccion de a en print: "<< &a <<std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	int a=12;
+	std::cout <<"Direccion de a en main: "<< &a <<std::endl;
+	print_message(a);
+	return 0;
+}
